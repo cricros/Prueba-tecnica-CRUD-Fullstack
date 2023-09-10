5c3cf244fb28ecdc75f25d3db356c47d4ad63bfa
cricros <vce11987@innovaccion.mx>
2023-09-10 16:27:40 -0600

    adding bootstrap and new endpoints

--- demo/src/main/java/com/SpringCourse/demo/controllers/userController.java
+++ demo/src/main/java/com/SpringCourse/demo/controllers/userController.java
@@ -1,5 +1,7 @@
 package com.SpringCourse.demo.controllers;
 
+import com.SpringCourse.demo.models.User;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +18,47 @@ public class userController {
     @RequestMapping(value = "json")
     public List<String> json(){
         return List.of("Manzana", "Perrito", "Perro");
+    }
+
+    // declaring an endpoint with params
+    @RequestMapping(value = "getUser/{id}")
+    public User getUser(@PathVariable Long id){
+        User user = new User();
+        user.setId(id);
+        user.setName("Pollito");
+        user.setLastName("En fuga");
+        user.setEmail("pollitoenfuga@gmail.com");
+        user.setTelephone(667687565);
+        return user;
+    }
+
+    @RequestMapping(value = "editUser")
+    public User editUser(){
+        User user = new User();
+        user.setName("Pollito");
+        user.setLastName("En fuga");
+        user.setEmail("pollitoenfuga@gmail.com");
+        user.setTelephone(667687565);
+        return user;
+    }
+
+    @RequestMapping(value = "deleteUser")
+    public User deleteUser(){
+        User user = new User();
+        user.setName("Pollito");
+        user.setLastName("En fuga");
+        user.setEmail("pollitoenfuga@gmail.com");
+        user.setTelephone(667687565);
+        return user;
+    }
+
+    @RequestMapping(value = "searchUser")
+    public User searchUser(){
+        User user = new User();
+        user.setName("Pollito");
+        user.setLastName("En fuga");
+        user.setEmail("pollitoenfuga@gmail.com");
+        user.setTelephone(667687565);
+        return user;
     }
 }
---
+++ demo/src/main/java/com/SpringCourse/demo/models/User.java
@@ -0,0 +1,62 @@
+package com.SpringCourse.demo.models;
+
+
+// declaring a model for a new user using name, lastName, email, telephone number and password as fields
+public class User {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private Integer telephone;
+    private String password;
+
+    // creating setters and getters
+
+    // getter
+    public Long getId() {
+        return id;
+    }
+    // setter
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(Integer telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
