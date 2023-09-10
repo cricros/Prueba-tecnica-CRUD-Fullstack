9812f1f5e911ac54959f3c9f6b11395a35499106
cricros <vce11987@innovaccion.mx>
2023-09-10 02:11:55 -0600

    fix commit spring boot version

---
+++ demo/src/test/java/com/SpringCourse/demo/SpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringCourse.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
