7cd26a416f5deb46011088362c02c1fd7480d698
cricros <vce11987@innovaccion.mx>
2023-09-10 19:21:33 -0600

    displaying all users from a list using querySelector and ajax

--- demo/src/main/resources/static/js/users.js
+++ demo/src/main/resources/static/js/users.js
@@ -1,18 +1,29 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  loadUser();
+  loadUsers();
   $('#usersTable').DataTable();
 });
 
-async function loadUser(){
-  const userResponse = await fetch('getUser/2221', {
+// async function to load all users using AJAX to consume an REST endpoint with fetch
+async function loadUsers(){
+  const userResponses = await fetch('getUsers', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
   });
-  const userContent = await userResponse.json();
-  console.log(userContent);
+  const userContents = await userResponses.json();
+  let userDataRowHTML = '';
+  for (let userContent of userContents) {
+  // declaring a new variable to concat HTML with the values from userResponses
+      let userDataRow = '<tr><td>' + userContent.id + '</td><td>' + userContent.name + ' ' + userContent.lastName +
+      '</td><td>' + userContent.email + '</td><td>' + userContent.telephone + '</td><td><div style="text-align: center;">'
+      + '<a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></div></td></tr>'
+      userDataRowHTML += userDataRow
+      }
+      // declaring a querySelector to be able to see all values in the same table
+  document.querySelector('#usersTable tbody').outerHTML = userDataRowHTML;
+
 }
 
